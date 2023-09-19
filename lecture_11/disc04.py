92a70c94d4bc75d5d07c5758e09d86cee024475a
Zhou HaoRan <90096601+Zhouhaoran1994@users.noreply.github.com>
2023-09-20 00:02:52 +0800

    lecture 11 Q7

--- lecture_11/disc04.py
+++ lecture_11/disc04.py
@@ -1,14 +1,22 @@
+from tree import *
+
 # Q7
 def find_path(t, x):
     """
+    >>> simplest = tree(2)
+    >>> find_path(simplest, 2)
+    [2]
+    >>> harder = tree(2, [tree(1), tree(15)])
+    >>> find_path(harder, 15)
+    [2, 15]
     >>> t = tree(2, [tree(7, [tree(3), tree(6, [tree(5), tree(11)])] ), tree(15)])
     >>> find_path(t, 5)
     [2, 7, 6, 5]
     >>> find_path(t, 10)  # returns None
     """
-    if _____________________________:
-        return _____________________________
-    _____________________________:
-        path = ______________________
-        if _____________________________:
-            return _____________________________
+    if label(t) == x:
+        return [x]
+    for b in branches(t):
+        path = find_path(b, x) # recursive leap of faith # [15]
+        if path:
+            return [label(t)] + path
